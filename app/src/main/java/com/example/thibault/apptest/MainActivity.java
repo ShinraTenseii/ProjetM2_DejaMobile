75cc21d89958898b295a9e74f9e779b9f8672934
thibault <thibault.leclair@gmail.com>
2019-02-12 15:21:15 +0100

    génération de clés

--- app/src/main/java/com/example/thibault/apptest/MainActivity.java
+++ app/src/main/java/com/example/thibault/apptest/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.thibault.apptest;
 
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.security.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +22,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 final TextView textDebug = findViewById(R.id.textViewDebug);
-                textDebug.setText("Clicked !");
+                try {
+                    textDebug.setText(genKeys());
+                } catch (NoSuchProviderException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (InvalidAlgorithmParameterException e) {
+                    e.printStackTrace();
+                }
             }
         });
+    }
+
+    protected String genKeys() throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_EC, "AndroidKeyStore");
+        kpg.initialize(new KeyGenParameterSpec.Builder(
+                "keypair",
+                KeyProperties.PURPOSE_SIGN | KeyProperties.PURPOSE_VERIFY)
+                .setDigests(KeyProperties.DIGEST_SHA256,
+                        KeyProperties.DIGEST_SHA512)
+                .build());
+
+        KeyPair kp = kpg.generateKeyPair();
+        return kp.getPublic().toString();
     }
 }
